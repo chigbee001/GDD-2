d24d2f70f83b168d0384a8d721c9646cb177b1f7
Geracedf <54971612+Geracedf@users.noreply.github.com>
2021-10-05 19:58:19 -0400

    Finished Projectile and added bullet tags

--- P Cubed/Assets/Scripts/Projectile.cs
+++ P Cubed/Assets/Scripts/Projectile.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float speed { get; set; }
+    public Vector3 direction { get; set; }
+    public Player player;
+    private Vector3 target;
+    private float aliveTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = direction * 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        aliveTimer += Time.deltaTime;
+
+        if(aliveTimer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Makes player bullets kill enemies and enemy bullets deal damage to players
+        switch (gameObject.tag)
+        {
+            case "pBullet":
+                if(other.tag == "Enemy")
+                {
+                    Destroy(other.gameObject);   
+                }
+                Destroy(gameObject);
+                break;
+            case "eBullet":
+                if(other.tag == "Player")
+                {
+                    player.TakeDamage();
+                }
+                Destroy(gameObject);
+                break;
+            default:
+                Debug.Log("PUT THE TAG IN THE BULLET PLS");
+                break;
+        }
     }
 }
