0ab17da1bfce6a2f6b2376bdb2609117ea1af185
dek6613 <dek6613@rit.edu>
2021-11-17 18:03:45 -0500

    Created basic outline for the baseBoss class

--- i have no ammo/Assets/Scripts/baseBoss.cs
+++ i have no ammo/Assets/Scripts/baseBoss.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class baseBoss : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    Vector2 direction = Vector2.up;
+
+    [SerializeField]
+    projectile projectilePrefab;
+
+    [SerializeField]
+    float maxHealth;
+    public float currentHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BasicMovement();
+
+        if (currentHealth <= 0) { Die(); }
+    }
+
+    void BasicMovement()
+    {
+        if (transform.position.y > 4) { direction = Vector2.down; }
+        if (transform.position.y < -4) { direction = Vector2.up; }
+
+        rb.velocity = direction * speed;
+    }
+
+    void Die()
+    {
+        Debug.Log(name + " has been killed");
+
+        // TEMPORARY, JUST TO NOTE THAT THEY WOULD'VE DIED BUT RESETTING THEIR HEALTH SO IT DOESN'T SPAM THE MESSAGE EVERY FRAME
+        currentHealth = maxHealth;
     }
 }
