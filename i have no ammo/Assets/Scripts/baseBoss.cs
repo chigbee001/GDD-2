a1024b4856c2766cc61d8d8a0e5b89dc92b413f0
chigbee001 <54906182+chigbee001@users.noreply.github.com>
2021-11-18 00:24:47 -0500

    very simple boss added

--- i have no ammo/Assets/Scripts/baseBoss.cs
+++ i have no ammo/Assets/Scripts/baseBoss.cs
@@ -6,8 +6,12 @@ public class baseBoss : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    
+    public GameObject[] projectileSpawners;
+
     [SerializeField]
     float speed;
+
     [SerializeField]
     Vector2 direction = Vector2.up;
 
@@ -17,7 +21,9 @@ public class baseBoss : MonoBehaviour
     [SerializeField]
     float maxHealth;
     public float currentHealth;
-    
+    public int fireRate;
+    private float fireCooldown;
+    private bool holdFire;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +34,16 @@ public class baseBoss : MonoBehaviour
     void Update()
     {
         BasicMovement();
-
+        if(fireCooldown <= 0 && !holdFire)
+        {
+            holdFire = true;
+            StartCoroutine(Fire());
+            fireCooldown = fireRate;
+        }
+        else
+        {
+            fireCooldown -= Time.deltaTime;
+        }
         if (currentHealth <= 0) { Die(); }
     }
 
@@ -46,5 +61,28 @@ public class baseBoss : MonoBehaviour
 
         // TEMPORARY, JUST TO NOTE THAT THEY WOULD'VE DIED BUT RESETTING THEIR HEALTH SO IT DOESN'T SPAM THE MESSAGE EVERY FRAME
         currentHealth = maxHealth;
+    }
+    /// <summary>
+    /// Will take in variable of attack pattern which will be created in the future, using this parameter it will determine which group of mounts to fire from, how many bullets,
+    /// and what pattern for the bullets from each mount
+    /// Currently just fires sine wave bullets from two mounts
+    /// </summary>
+    IEnumerator Fire()
+    {
+        //will go through each spawner group and only fire from the ones called for currently will just run through all and fire from all
+        //foreach(GameObject[] spawnerGroup in projectileSpawners)
+        //{
+            //foreach(GameObject spawner in spawnerGroup)
+            foreach(GameObject spawner in projectileSpawners)
+            {
+                //while loop will use number of bullets variable from attack pattern to determine how many to fire currently will fire 10
+                for(int i = 0; i < 10; i++)
+                {
+                Instantiate(projectilePrefab, spawner.transform.position, spawner.transform.rotation);
+                yield return new WaitForSeconds(.5f);
+                }
+            }
+        holdFire = false;
+        //}
     }
 }
